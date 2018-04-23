ac7030f0e3c0c995e87e9a0c7f9dd9b54debd16d
Anton Ayvarov <ayvaroff@hotmail.com>
2018-04-23 17:52:45 +0300

    add server github link repository

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ import './index.scss';
 const Home = () => (
     <div className={"home"}>
         <h1>Sample react redux saga application</h1>
-        <p>To test it from the box you neet to use this server: <a href="#d">GitHub</a></p>
+        <p>To test it from the box you neet to use this server: <a href="https://github.com/TomRossA/node-js-book-store">GitHub</a></p>
     </div>
 );
 
