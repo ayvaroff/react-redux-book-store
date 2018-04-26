819d4cf6bf981682eb38260c7c7e2221cba028a4
Anton Ayvarov <ayvaroff@hotmail.com>
2018-04-26 15:49:07 +0300

    update Home screen

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ import './index.scss';
 const Home = () => (
     <div className={"home"}>
         <h1>Sample react redux saga application</h1>
-        <p>To test it from the box you neet to use this server: <a href="https://github.com/TomRossA/node-js-book-store">GitHub</a></p>
+        <p>To test it from the box you neet to use on of this servers on GitHub: <a href="https://github.com/TomRossA/node-js-book-store">NodeJS</a> or <a href="https://github.com/TomRossA/python-flask-book-store">Python</a></p>
     </div>
 );
 
